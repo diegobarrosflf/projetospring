7539f1e7d9f1bbde3fba1dd6cd4f0b001efe2c5c
Diego Barros <diegobarrosflf@gmail.com>
2020-05-27 08:10:13 -0300

    Deletando Categoria com DELETE + Exception DataIntegrity.

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -49,4 +50,10 @@ public class CategoriaResource {
 
 	}
 
+	// deletando categoria
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return  ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.example.demo.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Categoria;
 import com.example.demo.repositories.CategoriaRepository;
+import com.example.demo.services.exception.DataIntegrityException;
 import com.example.demo.services.exception.ObjectNotFoundException;
 
 @Service
@@ -33,6 +35,14 @@ public class CategoriaService {
 		return repositorio.save(obj);
 	}
 	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repositorio.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
+	}
 	
 	
 	
